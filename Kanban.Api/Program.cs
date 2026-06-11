dd50561e7b72716374881952cc84477ed0c331c0
José Sebastián Delgado Gamez <142054398+ElGamezzz@users.noreply.github.com>
2026-06-11 11:27:33 -0500

    Proyecto Kanban completado: API + Blazor Server funcionando al 100%

--- Kanban.Api/Program.cs
+++ Kanban.Api/Program.cs
@@ -7,7 +7,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+        options.JsonSerializerOptions.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull;
+    });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
